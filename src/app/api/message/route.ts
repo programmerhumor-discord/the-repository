d9a8efc5807e986a5234f2c1b2349117dde78c5a
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-13 16:24:40 -0700

    fix: tree search algorithim

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,11 +4,17 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 	const { username, message } = await request.json();
 
-	if (!username || !message) {
+	if (
+		!username ||
+		!message ||
+		username.toString().trim() === "" ||
+		message.toString().trim() === ""
+	) {
 		return NextResponse.json({
 			message: "No message or username specified"
 		});
 	}
+
 	create_message(username.substring(0, 10), message.substring(0, 150));
 	return NextResponse.json({ message: "OK" });
 }

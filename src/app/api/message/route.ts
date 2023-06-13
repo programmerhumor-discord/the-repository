36081cf4705427576fe6e0f9a63634176926f7a9
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-13 16:11:23 -0700

    fix

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 	const { username, message } = await request.json();
 
+	if (!username || !message) {
+		return NextResponse.json({
+			message: "No message or username specified"
+		});
+	}
 	create_message(username.substring(0, 10), message.substring(0, 150));
 	return NextResponse.json({ message: "OK" });
 }

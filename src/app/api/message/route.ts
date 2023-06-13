53c7a5857eb770cf35d9a08c9e365bd55e1e77c8
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-13 16:07:05 -0700

    add: HARD limit on messages

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 	const { username, message } = await request.json();
 
-	create_message(username, message);
+	create_message(username.substring(0, 10), message.substring(0, 150));
 	return NextResponse.json({ message: "OK" });
 }

